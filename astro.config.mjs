7f94eb2c00181646a4e24b2afa52da5facf23bb4
churrezno <jorge@devala.es>
2026-04-30 18:27:05 +0200

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://ecamforum.es',
   base: process.env.BASE_PATH || '/',
   devToolbar: {
     enabled: false,
